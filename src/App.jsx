2f689d7a6d8d4c06b52a3e58fbef26e63843e009
Annie Pan <anniepan@Annies-MBP.fios-router.home>
2023-12-20 03:21:21 -0500

    finished 'Puppy Data with useState' tab

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-
+import {puppyList} from './data.js'
 import './App.css'
+import { useState } from 'react'
 
 function App() {
-  
-
+const [puppies, setPuppies] = useState(puppyList)
+console.log(puppies)
   return (
     <>
-      <div>
-       
+      <div className='App'>
+       {puppies.map(puppy => <p key={puppy.id}>{puppy.name}</p>)}
       </div>
   
     </>
