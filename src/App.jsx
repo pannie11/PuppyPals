787189974519e4f9a1d75e8941884fe8ca5cdc50
Annie Pan <anniepan@Annies-MBP.fios-router.home>
2023-12-20 03:49:17 -0500

    completed the 'Select a Featured Puppy' tab

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,23 @@ import { useState } from 'react'
 function App() {
 const [puppies, setPuppies] = useState(puppyList)
 console.log(puppies)
+
+const [featPupId, setFeatPupId] = useState(null)
+
+const featuredPup = puppies.find(pup => pup.id === featPupId)
+console.log(featuredPup)
   return (
     <>
       <div className='App'>
-       {puppies.map(puppy => <p key={puppy.id}>{puppy.name}</p>)}
+       {puppies.map(puppy => <p onClick={() => {setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>)}
+       {featPupId && 
+       <div>
+        <h2>{featuredPup.name}</h2>
+        <ul>
+          <li>Age: {featuredPup.age}</li>
+          <li>Email: {featuredPup.email}</li>
+        </ul>
+        </div>}
       </div>
   
     </>
